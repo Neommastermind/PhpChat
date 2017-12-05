b98ced5297f17ac80f783bc021fe537820319922
Neommastermind <31413602+Neommastermind@users.noreply.github.com>
2017-12-04 23:49:26 -0700

    More changes

---
+++ v1/User.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Matthew Jensen
+ * Date: 12/4/2017
+ * Time: 4:21 PM
+ */
+
+require_once 'DatabaseConnection.php';
+require_once 'PHP/ChatFunctions.php';
+
+class User
+{
+    private $userName;
+    private $password;
+    private $color;
+
+    /*
+     * Populates the user with data and sanitizes the input.
+     */
+    public function __construct($userName, $password, $color = "") {
+        if(!empty($userName) && !empty($password)) {
+            $this->setUserName($userName);
+            $this->setPassword($password);
+            $this->setColor($color);
+        }
+        else {
+            alert("You need to enter a user name and password!");
+            displayLoginForm();
+            exit(0);
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserName()
+    {
+        return $this->userName;
+    }
+
+    /**
+     * @param mixed $userName
+     */
+    public function setUserName($userName)
+    {
+        if(strlen($userName) <= 30 && strlen($userName) > 0) {
+            $this->userName = filter_var($userName, FILTER_SANITIZE_STRING);
+        }
+        else {
+            alert("Invalid User Name length.");
+            displayLoginForm();
+            exit(0);
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        if(strlen($password) <= 255 && strlen($password) > 0) {
+            $this->password = filter_var($password, FILTER_SANITIZE_STRING);
+        }
+        else {
+            alert("Invalid Password length.");
+            displayLoginForm();
+            exit(0);
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    /**
+     * @param mixed $color
+     */
+    public function setColor($color)
+    {
+        if(strlen($color) <= 7) {
+            $this->color = filter_var($color, FILTER_SANITIZE_STRING);
+        }
+        else {
+            alert("Invalid Color length.");
+            displayLoginForm();
+            exit(0);
+        }
+    }
+
+    /*
+     * returns a user from the database.
+     */
+    public static function loadUser($userName) {
+        try {
+            $db = DatabaseConnection::getInstance();
+            $stmtHandle = $db->prepare("SELECT userName, password, color FROM User WHERE userName = :userName");
+            $stmtHandle->bindValue('userName', $userName);
+            $stmtHandle->execute();
+            $stmtHandle->setFetchMode(PDO::FETCH_ASSOC);
+            $data = $stmtHandle->fetch();
+
+            $user = null;
+
+            if(!empty($data['userName']) && !empty($data['password']) && !empty($data['color'])) {
+                $user = new User($data['userName'], $data['password'], $data['color']);
+            }
+
+           return $user;
+        }
+        catch (PDOException $e) {
+            http_response_code(500);
+            throw $e;
+        }
+    }
+
+    /*
+     * Creates a user in the database and returns the results.
+     */
+    public static function createUser(User $user): bool {
+        try {
+            $db = DatabaseConnection::getInstance();
+            $stmtHandle = $db->prepare("INSERT INTO User (userName, password, color) VALUES(:userName, :password, :color)");
+            $stmtHandle->bindValue('userName', $user->getUserName());
+            $stmtHandle->bindValue('password', password_hash($user->getPassword(), PASSWORD_DEFAULT));
+            $stmtHandle->bindValue('color', $user->getColor());
+            return $stmtHandle->execute();
+        }
+        catch (PDOException $e) {
+            http_response_code(500);
+            throw $e;
+        }
+    }
+}
--- v1/chat.php
+++ v1/chat.php
@@ -1,76 +1,102 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: neomm
+ * User: Matthew Jensen
  * Date: 12/4/2017
- * Time: 8:08 AM
+ * Time: 5:26 PM
  */
 
-displayLoginForm();
+require_once 'User.php';
+require_once 'PHP/ChatFunctions.php';
 
-function displayLoginForm() {
-    $loginForm = '<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="description" content="This is a chat application using HTML5 WebSockets" />
-    <meta name="keywords" content="chat" />
-    <meta name="author" content="Matthew Jensen" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Title</title>
+$user;
+$submitType = "";
 
-    <link href="CSS/style.css" type="text/css" rel="stylesheet">
+//Make sure everything is working as intended
+if(array_key_exists('userName', $_POST) && array_key_exists('password', $_POST) && array_key_exists('color', $_POST) && array_key_exists('submit', $_POST)) {
+    if(!empty($_POST['submit'])) {
+        //Create the user object. The object sanitizes the data and does checking.
+        $user = new User($_POST['userName'], $_POST['password'], $_POST['color']);
+        $submitType = filter_var($_POST['submit'], FILTER_SANITIZE_STRING);
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-    <script src="JS/SocketHandler.js" type="text/javascript"></script>
-    <script src="JS/login.js" type="text/javascript"></script>
+    }
+    else {
+        http_response_code(400);
+        alert("Invalid data");
+        displayLoginForm();
+        exit(0);
+    }
+}
+else {
+    //You have not entered submitted the correct data or you have just opened file
+    //So you have to log in.
+    displayLoginForm();
+    exit(0);
+}
 
-</head>
-<body>
-    <div id="chatContainer">
-        <div id="loginMenu">
-            <h2>Login Menu</h2>
-            <form method="post" action="PHP/login.php">
-                <table>
-                    <tr>
-                        <td>
-                            <label for="userName">User Name: </label>
-                        </td>
-                        <td>
-                            <input type="text" id="userName" name="userName" placeholder="Type your user name here" maxlength="30" required/>
-                        </td>
-                    </tr>
-                    <tr>
-                    <td>
-                        <label for="color">User Name Color(Only if you are creating): </label>
-                    </td>
-                    <td>
-                        <input type="color" id="color" name="color"/>
-                    </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="password">Password: </label>
-                        </td>
-                        <td>
-                            <input type="password" name="password" id="password" placeholder="Type your password here" maxlength="30" required/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <input type="submit" name="submit" id="loginSubmit" value="Login"/>
-                        </td>
-                        <td>
-                            <input type="submit" name="submit" id="createSubmit" value="Create"/>
-                        </td>
-                    </tr>
-                </table>
-            </form>
-            </div>
-        </div>
-    </div>
-</body>
-</html>';
+if($submitType === 'Login') {
+    login($user);
+}
+else if($submitType === 'Create') {
+    create($user);
+}
+else {
+    http_response_code(400);
+    alert("You have sent an invalid submission type.");
+    displayLoginForm();
+    exit(0);
+}
 
-    echo $loginForm;
+/*
+ * Validates a users log in and redirects them appropriately.
+ */
+function login(User $user) {
+    try {
+        $databaseUser = User::loadUser($user->getUserName());
+
+        if (!empty($databaseUser) && password_verify($user->getPassword(), $databaseUser->getPassword()) === true) {
+            //Valid login!
+            displayChat($user->getUserName());
+            exit(0);
+        } else {
+            http_response_code(401);
+            alert("Invalid login information!");
+            displayLoginForm();
+            exit(0);
+        }
+    }
+    catch(PDOException $e) {
+        http_response_code(500);
+        alert("An error has occurred while trying to log in. $e");
+        displayLoginForm();
+        exit(0);
+    }
+}
+
+/*
+ * Creates a user in the database and redirects them appropriately.
+ */
+function create(User $user) {
+    try {
+        //Boolean status
+        $status = User::createUser($user);
+
+        if ($status) {
+            //Valid Creation, log in!
+            http_response_code(201);
+            displayChat($user->getUserName());
+            exit(0);
+        } else {
+            http_response_code(401);
+            alert("Invalid Creation information!");
+            displayLoginForm();
+            exit(0);
+        }
+    }
+    catch(PDOException $e) {
+        http_response_code(500);
+        alert("An error has occurred while trying to log in. $e");
+        displayLoginForm();
+        exit(0);
+    }
 }
